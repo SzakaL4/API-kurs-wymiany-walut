c7bc56f43386ef04e001b7206c8fedb5eb9f223c
Szymon <gosu4@interia.pl>
2023-03-12 21:01:57 +0100

    demo

---
+++ kurs wymiany walut API.py
@@ -0,0 +1,16 @@
+import requests
+def zapytaj():
+    url = f"http://api.nbp.pl/api/exchangerates/rates/A/{waluta}/?format=json"
+    response=requests.get(url)
+    response
+    return response.json()
+
+print('Kalkulator Walut')
+waluta=input('Podaj rodzaj Waluty: ')
+dane=zapytaj()
+data=dane['rates'] [0]['effectiveDate']
+rodzaj=dane['currency']
+kwota=dane['rates'][0]['mid']
+print(data)
+print(rodzaj)
+print(float(kwota))
